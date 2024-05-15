7e6e40c7eab62c23a7600e06be520f304601a6f1
Christopher Johnstone <chrisj97@proton.me>
2024-05-15 09:48:29 +0100

    Added default to start_tmux_session

--- start_tmux_session.sh
+++ start_tmux_session.sh
@@ -1,9 +1,15 @@
-session_name="${1##*/}"
+if [ -z $1 ]
+then
+    path="~/my_folder"
+else
+    path=$1
+fi
+session_name="${path##*/}"
 
 tmux has-session -t "$session_name"
 if [ $? != 0 ]
 then
-    tmux new-session -d -s "$session_name" -c "$1" -n "Editing"
-    tmux new-window -t "$session_name" -c "$1" -n "Testing"
+    tmux new-session -d -s "$session_name" -c "$path" -n "Editing"
+    tmux new-window -t "$session_name" -c "$path" -n "Testing"
 fi
 tmux switch-client -t "$session_name:Editing"
