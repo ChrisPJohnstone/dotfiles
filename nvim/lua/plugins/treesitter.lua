537225daaacecd6b671f1562be7b00cad7f0cc82
Christopher Johnstone <chrisj97@proton.me>
2024-06-14 23:40:19 +0100

    Added vimscript to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"lua", "python", "sql", "nix"},
+    ensure_installed = {"vim", "lua", "python", "sql", "nix"},
     sync_install = false,
     auto_install = true,
     highlight = {
