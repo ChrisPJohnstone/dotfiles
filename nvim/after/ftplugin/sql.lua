878e8006034e9703a42d73cc9df7ba2263de7b3f
Christopher Johnstone <chrisj97@proton.me>
2024-06-15 00:00:21 +0100

    Added sql ftplugin

---
+++ nvim/after/ftplugin/sql.lua
@@ -0,0 +1,6 @@
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+
+-- Add a vertical ruler at char 80 & make it light grey
+vim.opt_local.colorcolumn = "120"
+vim.cmd([[highlight ColorColumn ctermbg=8]])
