e92358815fe65807474a7db42a6b130854a0dda9
Christopher Johnstone <chrisj97@proton.me>
2024-06-20 20:48:16 +0100

    Added surround

--- nvim/lua/config/remaps.lua
+++ nvim/lua/config/remaps.lua
@@ -3,9 +3,3 @@ vim.api.nvim_set_keymap("n", "<C-W><C-h>", "<C-W><", {noremap = true})
 vim.api.nvim_set_keymap("n", "<C-W><C-j>", "<C-W>+", {noremap = true})
 vim.api.nvim_set_keymap("n", "<C-W><C-k>", "<C-W>-", {noremap = true})
 vim.api.nvim_set_keymap("n", "<C-W><C-l>", "<C-W>>", {noremap = true})
-
--- Command for encasing
-vim.g.mapleader = "\\"
-vim.api.nvim_set_keymap("n", "<leader>'", "ciw'<C-R>\"'<ESC>bh", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>\"", "ciw\"<C-R>\"\"<ESC>bh", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>`", "ciw`<C-R>\"`<ESC>bh", {noremap = true})
