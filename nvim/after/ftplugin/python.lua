062d36b1cba7644b0b99550bb2c52f6aeac09762
Christopher Johnstone <chrisj97@proton.me>
2024-06-26 12:31:02 +0100

    Fixed python indenting

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,6 +1,10 @@
+-- Set indenting
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 
 -- Add a vertical ruler at char 80 & make it light grey
 vim.opt_local.colorcolumn = "80"
 vim.cmd([[highlight ColorColumn ctermbg=8]])
+
+-- Set the python interpreter to python3
+vim.g.pyindent_open_paren = 4
