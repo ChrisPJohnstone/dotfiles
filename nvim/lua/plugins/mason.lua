592fc56039e1a9be8750ccf1f41a0045e51121db
Christopher Johnstone <chrisj97@proton.me>
2024-07-07 12:12:20 +0100

    Cleaned up dap

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,20 +1,9 @@
 return {
     "williamboman/mason.nvim",
-    config = function()
-        local mason = require("mason")
-
-        mason.setup {
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
-            ensure_installed = {
-                "debugpy",
-                "pyright",
-            },
-        }
-    end,
+    opts = {
+        ensure_installed = {
+            "debugpy",
+            "pyright",
+        },
+    },
 }
