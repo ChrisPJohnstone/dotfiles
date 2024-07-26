6e9bb84d4cf2fd76edbdfe923d76f6ed704f60b5
Christopher Johnstone <chrisj97@proton.me>
2024-07-26 20:10:10 +0100

    Let colorcolumn be default colour

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -4,7 +4,6 @@ vim.opt_local.tabstop = 4
 
 -- Add a vertical ruler at char 80 & make it light grey
 vim.opt_local.colorcolumn = "80"
-vim.cmd([[highlight ColorColumn ctermbg=8]])
 
 -- Set the python interpreter to python3
 vim.g.pyindent_open_paren = 4
