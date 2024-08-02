bf00b2624f81643009d0859dc7280a845ee1267b
Christopher Johnstone <chrisj97@proton.me>
2024-08-02 22:07:22 +0000

    Added script to do links in linux

---
+++ setup_linux_links.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+ln -s ~/configs/alacritty ~/.config/alacritty
+ln -s ~/configs/i3 ~/.config/i3
+ln -s ~/configs/i3status ~/.config/i3status
+ln -s ~/configs/nvim ~/.config/nvim
+ln -s ~/configs/vim ~/.vim
+ln -s ~/configs/.gitconfig ~/.gitconfig
+ln -s ~/configs/.tmux.conf ~/.tmux.conf
+ln -s ~/configs/.xinitrc ~/.xinitrc
+ln -s ~/configs/.zshrc ~/.zshrc
