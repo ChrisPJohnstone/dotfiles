49e28109c26c137786a4324116b99f4a3bd45f53
Christopher Johnstone <chrisj97@proton.me>
2024-08-21 20:32:42 +0100

    Moved Xresources to dir

--- setup_linux_links.sh
+++ setup_linux_links.sh
@@ -4,8 +4,8 @@ ln -s ~/configs/i3 ~/.config/i3
 ln -s ~/configs/i3status ~/.config/i3status
 ln -s ~/configs/nvim ~/.config/nvim
 ln -s ~/configs/vim ~/.vim
+ln -s ~/configs/X11 ~/.config/X11
 ln -s ~/configs/.gitconfig ~/.gitconfig
 ln -s ~/configs/.tmux.conf ~/.tmux.conf
-ln -s ~/configs/.Xresources ~/.Xresources
 ln -s ~/configs/.xinitrc ~/.xinitrc
 ln -s ~/configs/.zshrc ~/.zshrc
