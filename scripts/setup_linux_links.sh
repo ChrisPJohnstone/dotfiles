3d25de2d1fbda9518cf46abb6c7d0cd62d75ee4f
Christopher Johnstone <chrisj97@proton.me>
2024-08-22 23:12:08 +0100

    Moved everything to directories

--- scripts/setup_linux_links.sh
+++ scripts/setup_linux_links.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 ln -s ~/configs/X11 ~/.config/X11
 ln -s ~/configs/alacritty ~/.config/alacritty
+ln -s ~/configs/git/.gitconfig ~/.gitconfig
 ln -s ~/configs/i3 ~/.config/i3
 ln -s ~/configs/i3status ~/.config/i3status
 ln -s ~/configs/tmux ~/.config/tmux
 ln -s ~/configs/nvim ~/.config/nvim
 ln -s ~/configs/vim ~/.vim
-ln -s ~/configs/.gitconfig ~/.gitconfig
-ln -s ~/configs/.xinitrc ~/.xinitrc
-ln -s ~/configs/.zshrc ~/.zshrc
+ln -s ~/configs/xinit/xinitrc ~/.xinitrc
+ln -s ~/configs/zsh/.zshrc ~/.zshrc
