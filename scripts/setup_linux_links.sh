358c27bc1c2413bc34f426ed3fdb8ee85500728f
Christopher Johnstone <chrisj97@proton.me>
2024-08-22 23:17:35 +0100

    Consolidated xserver dirs

--- scripts/setup_linux_links.sh
+++ scripts/setup_linux_links.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-ln -s ~/configs/X11 ~/.config/X11
 ln -s ~/configs/alacritty ~/.config/alacritty
 ln -s ~/configs/git/.gitconfig ~/.gitconfig
 ln -s ~/configs/i3/base ~/.config/i3
@@ -7,5 +6,6 @@ ln -s ~/configs/i3/status ~/.config/i3status
 ln -s ~/configs/tmux ~/.config/tmux
 ln -s ~/configs/nvim ~/.config/nvim
 ln -s ~/configs/vim ~/.vim
-ln -s ~/configs/xinit/xinitrc ~/.xinitrc
+ln -s ~/configs/xserver/X11 ~/.config/X11
+ln -s ~/configs/xserver/xinitrc ~/.xinitrc
 ln -s ~/configs/zsh/.zshrc ~/.zshrc
