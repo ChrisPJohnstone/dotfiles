4a8a137d7c107500427324956d03886562e82665
Christopher Johnstone <chrisj97@proton.me>
2024-11-06 22:37:16 +0000

    Readded i3status

--- scripts/setup_linux_links.sh
+++ scripts/setup_linux_links.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/bash
 ln -s ~/configs/alacritty ~/.config/alacritty
 ln -s ~/configs/git ~/.config/git
-ln -s ~/configs/i3/base ~/.config/i3
-ln -s ~/configs/i3/status ~/.config/i3status
+ln -s ~/configs/i3status ~/.config/i3status
 ln -s ~/configs/redshift ~/.config/redshift
 ln -s ~/configs/rofi ~/.config/rofi
 ln -s ~/configs/sway ~/.config/sway
 ln -s ~/configs/tmux ~/.config/tmux
 ln -s ~/configs/nvim ~/.config/nvim
 ln -s ~/configs/vim ~/.vim
-ln -s ~/configs/xserver/X11 ~/.config/X11
-ln -s ~/configs/xserver/xinitrc ~/.xinitrc
 ln -s ~/configs/zsh/.zshrc ~/.zshrc
