5837e6ea5dbeb0f9af587be55ddcca66a6354737
Christopher Johnstone <chrisj97@proton.me>
2024-12-06 18:48:29 +0000

    Added mako config

--- scripts/setup_linux_links.sh
+++ scripts/setup_linux_links.sh
@@ -3,6 +3,7 @@ ln -s ~/configs/alacritty   ~/.config/alacritty
 ln -s ~/configs/gammastep   ~/.config/gammastep
 ln -s ~/configs/git         ~/.config/git
 ln -s ~/configs/i3status    ~/.config/i3status
+ln -s ~/configs/mako        ~/.config/mako
 ln -s ~/configs/nvim        ~/.config/nvim
 ln -s ~/configs/rofi        ~/.config/rofi
 ln -s ~/configs/sway        ~/.config/sway
