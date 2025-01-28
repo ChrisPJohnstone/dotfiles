205df7846cde2f175dadfc692f4fb4d4c047d849
Christopher Johnstone <chrisj97@proton.me>
2025-01-28 09:55:43 +0000

    Added binding for open dir

--- nvim/lua/config/bindings.lua
+++ nvim/lua/config/bindings.lua
@@ -1,3 +1,6 @@
+-- Open dir of current file
+vim.api.nvim_set_keymap("n", "<leader>d", ":e %:h<CR>", {noremap = true})
+
 -- Window resizing using Ctrl + hjkl
 vim.api.nvim_set_keymap("n", "<C-W><C-h>", "<C-W><", {noremap = true})
 vim.api.nvim_set_keymap("n", "<C-W><C-j>", "<C-W>+", {noremap = true})
