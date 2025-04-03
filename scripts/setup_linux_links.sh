95d1c36e43cc6b2be40e72e50152facaf0682ef2
Christopher Johnstone <chrisj97@proton.me>
2025-04-03 18:26:59 +0100

    Removed sway

--- scripts/setup_linux_links.sh
+++ scripts/setup_linux_links.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/bash
 ln -s ~/configs/alacritty   ~/.config/alacritty
-ln -s ~/configs/gammastep   ~/.config/gammastep
 ln -s ~/configs/git         ~/.config/git
 ln -s ~/configs/gtk-3.0     ~/.config/gtk-3.0
-ln -s ~/configs/i3status    ~/.config/i3status
-ln -s ~/configs/mako        ~/.config/mako
 ln -s ~/configs/nvim        ~/.config/nvim
 ln -s ~/configs/qtile       ~/.config/qtile
 ln -s ~/configs/rofi        ~/.config/rofi
-ln -s ~/configs/sway        ~/.config/sway
 ln -s ~/configs/tmux        ~/.config/tmux
 ln -s ~/configs/vim         ~/.vim
-ln -s ~/configs/waybar      ~/.config/waybar
 ln -s ~/configs/zsh/zshrc   ~/.zshrc
