0960acb0796afc2248b2de7b1b1ed794c0ad505c
Christopher Johnstone <chrisj97@proton.me>
2025-04-19 09:01:12 +0100

    Added save shortcut

--- nvim/lua/config/bindings.lua
+++ nvim/lua/config/bindings.lua
@@ -4,6 +4,9 @@ vim.g.mapleader = " "
 -- Open dir of current file
 vim.api.nvim_set_keymap("n", "<leader>d", ":e %:h<CR>", {noremap = true})
 
+-- Save
+vim.api.nvim_set_keymap("n", "<leader>s", ":w<CR>", {noremap = true})
+
 -- Window resizing using Ctrl + hjkl
 vim.api.nvim_set_keymap("n", "<C-W><C-h>", "<C-W><", {noremap = true})
 vim.api.nvim_set_keymap("n", "<C-W><C-j>", "<C-W>+", {noremap = true})
