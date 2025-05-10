0aba55ecb9a578792ba115574c21c2fe049f20a3
Christopher Johnstone <chrisj97@proton.me>
2025-05-10 16:17:49 +0100

    Added some configs to custom filetypes

--- nvim/lua/settings/filetypes.lua
+++ nvim/lua/settings/filetypes.lua
@@ -1,2 +1,6 @@
+-- Configs
+vim.cmd("autocmd BufRead,BufNewFile */git/config set filetype=gitconfig")
+vim.cmd("autocmd BufRead,BufNewFile */tmux/*.conf set filetype=tmux")
+
 -- SQL Files
 vim.cmd("autocmd BufRead,BufNewFile *.ddl set filetype=sql")
