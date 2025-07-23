712c6195abe16713de5629ee81b014b4f01aa7de
Christopher Johnstone <chrisj97@proton.me>
2025-07-23 20:35:59 +0100

    Added keybind for terminal split

--- nvim/lua/bindings/splits.lua
+++ nvim/lua/bindings/splits.lua
@@ -6,3 +6,10 @@ vim.api.nvim_set_keymap("n", "<C-W><C-h>", "<C-W><", {noremap = true})
 vim.api.nvim_set_keymap("n", "<C-W><C-j>", "<C-W>+", {noremap = true})
 vim.api.nvim_set_keymap("n", "<C-W><C-k>", "<C-W>-", {noremap = true})
 vim.api.nvim_set_keymap("n", "<C-W><C-l>", "<C-W>>", {noremap = true})
+
+-- Open terminal in bottom split
+vim.keymap.set("n", "<C-W>t", function()
+  vim.cmd.new()
+  vim.cmd.wincmd("J")
+  vim.cmd.term()
+end)
