579c003088f703d9e89210b335aeb1790bc40b27
Christopher Johnstone <chrisj97@proton.me>
2025-08-01 21:24:06 +0100

    Fixed line numbers flickering in terminal/help pages

--- nvim/lua/settings/line_numbers.lua
+++ nvim/lua/settings/line_numbers.lua
@@ -1,14 +1,30 @@
--- Enable line numbering
-vim.opt.number = true
+-- Group
+vim.api.nvim_create_augroup("LineNumbers", { clear = true })
+
+-- Enable line numbering in terminal
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+  group = "LineNumbers",
+  callback = function()
+    if vim.bo.buflisted then
+      vim.opt_local.number = true
+      vim.opt_local.relativenumber = true
+    end
+  end
+})
 
 -- Enable relative line numbering for focused window
 vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "WinEnter" }, {
+  group = "LineNumbers",
   pattern = { "*" },
   callback = function()
+    vim.opt.number = true
     vim.opt.relativenumber = true
   end,
 })
+
+-- Disable relative numbers when leaving a window
 vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "WinLeave" }, {
+  group = "LineNumbers",
   pattern = { "*" },
   callback = function()
     vim.opt.relativenumber = false
