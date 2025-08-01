1741042dd638cc969ce69c74973fb69faf64aaa0
Christopher Johnstone <chrisj97@proton.me>
2025-08-01 19:41:59 +0100

    Fixed warnings for vim globals in luals

--- nvim/lsp/luals.lua
+++ nvim/lsp/luals.lua
@@ -1,5 +1,18 @@
 return {
 	cmd = { "lua-language-server" },
 	filetypes = { "lua" },
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+		},
+	},
 }
 -- Dependencies `lua-language-server`
