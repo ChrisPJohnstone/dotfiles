da378dfc2d347b536d3ae41a5be555d8c2293955
Christopher Johnstone <chrisj97@proton.me>
2025-08-07 15:59:22 +0100

    Changed colorscheme to zaibatsu

--- nvim/lua/settings/init.lua
+++ nvim/lua/settings/init.lua
@@ -9,6 +9,9 @@ require("settings.tab")
 require("settings.wildignore")
 require("settings.wildmenu")
 
+-- Set colorscheme
+vim.cmd("colorscheme zaibatsu")
+
 -- Enable syntax highlighting
 vim.cmd("syntax on")
 
