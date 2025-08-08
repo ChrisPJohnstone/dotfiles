b720242a2e02c3b33955e7e9af5ffd038bf5d5b1
Christopher Johnstone <chrisj97@proton.me>
2025-08-08 19:47:32 +0100

    Rebound copilot accept to Ctrl + Y

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,1 +1,9 @@
-return { "github/copilot.vim" }
+return {
+  "github/copilot.vim",
+  config = function()
+    vim.keymap.set('i', '<C-Y>', 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+    })
+  end,
+}
