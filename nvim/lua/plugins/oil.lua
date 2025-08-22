86eddcfa8d96b0132da841c18bae1b73bf6184fb
Christopher Johnstone <chrisj97@proton.me>
2025-08-22 09:23:21 +0100

    Moved current directory binding to -

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -6,4 +6,12 @@ return {
       show_hidden = true,
     },
   },
+  config = function()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,
+      },
+    })
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end
 }
