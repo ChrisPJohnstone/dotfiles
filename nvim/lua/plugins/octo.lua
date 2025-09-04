b50a613ca6df5f342c264e04b1c6956eb48f2c69
Christopher Johnstone <chrisj97@proton.me>
2025-09-04 22:23:59 +0100

    Fixed octo nvim

--- nvim/lua/plugins/octo.lua
+++ nvim/lua/plugins/octo.lua
@@ -3,6 +3,6 @@ vim.pack.add({ "https://github.com/nvim-telescope/telescope.nvim" })
 vim.pack.add({ "https://github.com/nvim-lua/plenary.nvim" })
 local os = vim.loop.os_uname().sysname
 if os ~= "Linux" then
-  vim.pack.add({ "https://github.com/OctoVim/OctoVim" })
+  vim.pack.add({ "https://github.com/pwntester/octo.nvim" })
   require("octo").setup()
 end
