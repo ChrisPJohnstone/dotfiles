8d18cf7e7bf0363e8a27f0b2a0151b19d42098e6
Christopher Johnstone <chrisj97@proton.me>
2025-09-04 22:24:39 +0100

    Removed os lock from octo

--- nvim/lua/plugins/octo.lua
+++ nvim/lua/plugins/octo.lua
@@ -1,8 +1,5 @@
 vim.pack.add({ "https://github.com/nvim-tree/nvim-web-devicons" })
 vim.pack.add({ "https://github.com/nvim-telescope/telescope.nvim" })
 vim.pack.add({ "https://github.com/nvim-lua/plenary.nvim" })
-local os = vim.loop.os_uname().sysname
-if os ~= "Linux" then
-  vim.pack.add({ "https://github.com/pwntester/octo.nvim" })
-  require("octo").setup()
-end
+vim.pack.add({ "https://github.com/pwntester/octo.nvim" })
+require("octo").setup()
