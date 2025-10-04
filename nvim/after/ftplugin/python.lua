520777947b8af3dac3e87e30ebf22ff8ebb333e0
Christopher Johnstone <chrisj97@proton.me>
2025-10-04 22:41:50 +0100

    Updated python indent settings

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -2,11 +2,16 @@
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 
+-- Fix python indenting for open parens
+vim.g.python_indent = {
+  open_paren = 'shiftwidth()',
+  close_paren = 'shiftwidth()',
+  nested_paren = 'shiftwidth()',
+  continue = 'shiftwidth()',
+}
+
 -- Add a vertical ruler at char 80 & make it light grey
 vim.opt_local.colorcolumn = "80"
-
--- Set the python interpreter to python3
-vim.g.pyindent_open_paren = 4
 
 -- Run pytest
 vim.keymap.set("n", "<leader>t", ":w | !pytest %<CR>", { noremap = true })
