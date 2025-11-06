729d76e86e644a4ab5f42f8272421a310706ecac
Christopher Johnstone <chrisj97@proton.me>
2025-11-06 19:11:01 +0000

    Refactor LSP autocommands

--- nvim/lua/settings/lsp.lua
+++ nvim/lua/settings/lsp.lua
@@ -23,66 +23,84 @@ vim.lsp.enable({
 -- LSP augroup
 local lsp_augroup = vim.api.nvim_create_augroup("LSP", {})
 
--- Autoformat
+function LSPFormatOnSave(client, buf)
+  if
+      not client:supports_method("textDocument/formatting")
+      and client:supports_method("textDocument/willSaveWaitUntil")
+  then
+    return
+  end
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = lsp_augroup,
+    buffer = buf,
+    callback = function()
+      vim.lsp.buf.format({ bufnr = buf, id = client.id, timeout_ms = 1000 })
+    end,
+  })
+end
+
+function LSPCompletion(client, buf)
+  if not client:supports_method("textDocument/completion") then
+    return
+  end
+  vim.opt.completeopt = {
+    "menu",
+    "menuone",
+    "noinsert",
+    "noselect",
+    "fuzzy",
+    "popup",
+  }
+  vim.lsp.completion.enable(true, client.id, buf, { autotrigger = true })
+end
+
+function LSPInLineCompletion(client, buf)
+  if not client:supports_method(vim.lsp.protocol.Methods.textDocument_inlineCompletion, buf) then
+    return
+  end
+  vim.lsp.inline_completion.enable(true, { bufnr = buf })
+  vim.keymap.set(
+    'i',
+    '<C-F>',
+    vim.lsp.inline_completion.get,
+    { desc = 'LSP: accept inline completion', buffer = buf }
+  )
+  vim.keymap.set(
+    'i',
+    '<C-G>',
+    vim.lsp.inline_completion.select,
+    { desc = 'LSP: switch inline completion', buffer = buf }
+  )
+end
+
+function LSPDocumentHighlight(client, buf)
+  if vim.fn.mode() == "i" then
+    vim.lsp.buf.clear_references()
+    return
+  end
+  local supports_highlight = false
+  local clients = vim.lsp.get_clients({ bufnr = 0 })
+  for _, client in ipairs(clients) do
+    if client.server_capabilities.documentHighlightProvider then
+      supports_highlight = true
+      break
+    end
+  end
+  if not supports_highlight then
+    return
+  end
+  vim.lsp.buf.clear_references()
+  vim.lsp.buf.document_highlight()
+end
+
 vim.api.nvim_create_autocmd("LspAttach", {
   group = lsp_augroup,
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    if
-        not client:supports_method("textDocument/willSaveWaitUntil")
-        and client:supports_method("textDocument/formatting")
-    then
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-        end,
-      })
-    end
-  end,
-})
-
--- Autocomplete
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = lsp_augroup,
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    if client:supports_method("textDocument/completion") then
-      vim.opt.completeopt = {
-        "menu",
-        "menuone",
-        "noinsert",
-        "noselect",
-        "fuzzy",
-        "popup",
-      }
-      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-    end
-  end,
-})
-
--- In-line Completion
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local bufnr = args.buf
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlineCompletion, bufnr) then
-      vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
-
-      vim.keymap.set(
-        'i',
-        '<C-F>',
-        vim.lsp.inline_completion.get,
-        { desc = 'LSP: accept inline completion', buffer = bufnr }
-      )
-      vim.keymap.set(
-        'i',
-        '<C-G>',
-        vim.lsp.inline_completion.select,
-        { desc = 'LSP: switch inline completion', buffer = bufnr }
-      )
-    end
+    local buf = args.buf
+    LSPFormatOnSave(client, buf)
+    LSPCompletion(client, buf)
+    LSPInLineCompletion(client, buf)
   end
 })
 
