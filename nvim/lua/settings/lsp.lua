bde93dbb12db33267b70a0bdecf45eabfcc9e7ea
Christopher Johnstone <chrisj97@proton.me>
2025-11-06 18:55:45 +0000

    Added document highlighting

--- nvim/lua/settings/lsp.lua
+++ nvim/lua/settings/lsp.lua
@@ -85,3 +85,26 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
   end
 })
+
+-- Document Highlighting
+vim.api.nvim_create_autocmd("CursorMoved", {
+  callback = function()
+    if vim.fn.mode() == "i" then
+      vim.lsp.buf.clear_references()
+      return
+    end
+    local supports_highlight = false
+    local clients = vim.lsp.get_clients({ bufnr = 0 })
+    for _, client in ipairs(clients) do
+      if client.server_capabilities.documentHighlightProvider then
+        supports_highlight = true
+        break
+      end
+    end
+    if not supports_highlight then
+      return
+    end
+    vim.lsp.buf.clear_references()
+    vim.lsp.buf.document_highlight()
+  end
+})
