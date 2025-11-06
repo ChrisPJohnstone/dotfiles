f5bdfc7ff6473f2e5e6d81307beeba5d1f07802a
Christopher Johnstone <chrisj97@proton.me>
2025-11-06 21:14:37 +0000

    Added gitsigns

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,2 @@
+vim.pack.add({ "https://github.com/lewis6991/gitsigns.nvim" })
+vim.keymap.set('n', '<leader>gd', "<cmd>Gitsigns diffthis HEAD<CR>")
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,6 +1,7 @@
 require("plugins.mason")
 
 require("plugins.fugitive")
+require("plugins.gitsigns")
 require("plugins.mini-comment")
 require("plugins.mini-diff")
 require("plugins.mini-hipatterns")
