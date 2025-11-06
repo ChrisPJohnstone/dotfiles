fb3576d853fc609120e6ffa603613751d56d8003
Christopher Johnstone <chrisj97@proton.me>
2025-11-06 21:34:28 +0000

    Disabled gitsigns symbols

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,2 +1,9 @@
 vim.pack.add({ "https://github.com/lewis6991/gitsigns.nvim" })
+
+-- Disable everything, I just want the diffthis command
+require("gitsigns").setup({
+  signs_staged_enable = false,
+  signcolumn = false,
+})
+
 vim.keymap.set('n', '<leader>gd', "<cmd>Gitsigns diffthis HEAD<CR>")
