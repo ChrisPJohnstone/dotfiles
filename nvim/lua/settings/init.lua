3a06ec0b06b95ccd3ff2887472ceaba54d5da544
Christopher Johnstone <chrisj97@proton.me>
2026-03-14 10:15:02 +0000

    Added all notes to autocommit

--- nvim/lua/settings/init.lua
+++ nvim/lua/settings/init.lua
@@ -38,6 +38,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- Commit notes on save
 vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = vim.fn.expand("~/notes/daily/") .. "*.md",
+  pattern = vim.fn.expand("~/notes/") .. "*.md",
   command = "silent !cd %:~:h && git add %:t && git commit -m 'Updated notes' && git push",
 })
