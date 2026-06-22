133dbd284efe9daf43e81ed292404b60c74e05d4
Christopher Johnstone <chrisj97@proton.me>
2026-06-22 21:49:59 +0100

    Added some obsidian bindings

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -10,8 +10,35 @@ require("obsidian").setup({
   daily_notes = {
     folder = "daily",
   },
+  notes_subdir = "scratch",
+  follow_url_func = function(url)
+    vim.ui.open(url)
+  end,
+  note_id_func = function(title)
+    if title == nil then
+      return tostring(os.time())
+    end
+    return title:gsub("[^a-zA-Z0-9 ]", ""):gsub(" ", "_"):lower()
+  end,
   completion = {
-    nvim_cmp = false,
+    nvim_cmp = true,
     min_chars = 2,
   },
+  mappings = {
+    -- Follow link under cursor, passthrough to normal gf if not a link
+    gf = {
+      action = function()
+        return require("obsidian").util.gf_passthrough()
+      end,
+      opts = { noremap = false, expr = true, buffer = true },
+    },
+  },
 })
+vim.keymap.set("n", "<leader>nf", ":ObsidianQuickSwitch<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>ng", ":ObsidianSearch<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>nh", ":ObsidianTOC<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>nn", function()
+  local subject = vim.fn.input("Note: ")
+  if subject == "" then return end
+  vim.cmd("ObsidianNew " .. subject)
+end, { desc = "Obsidian: new note" })
