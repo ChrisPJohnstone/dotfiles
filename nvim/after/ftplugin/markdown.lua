ef0dfe148c506c9f4711c89c199cc76c9fa85336
Christopher Johnstone <chrisj97@proton.me>
2026-07-10 12:15:34 +0100

    feat: updated markdown settings

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,10 @@
 -- Required for Obsidian
 vim.opt.conceallevel = 1
+
+-- Indentation settings
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+
+-- Fold Settings
+vim.opt_local.foldenable = true
+vim.opt_local.foldlevel = 0
