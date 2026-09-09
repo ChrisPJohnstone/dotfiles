88f2b5f0663a2789bea21603429d9719bd476cd8
Christopher Johnstone <chrisj97@proton.me>
2026-09-09 09:46:17 +0100

    feat: added jira link shortener

--- nvim/lua/bindings/init.lua
+++ nvim/lua/bindings/init.lua
@@ -5,3 +5,11 @@ require("bindings.splits")
 
 -- Save focused file
 vim.keymap.set("n", "<leader>s", ":w<CR>", { noremap = true })
+
+-- Convert Jira URL to markdown link: https://*.atlassian.net/browse/VEGA-1040 → [VEGA-1040](url)
+vim.keymap.set(
+  "n",
+  "<leader>j",
+  ":s/\\(](\\)\\@<!\\(https:\\/\\/[^\\.]*\\.atlassian\\.net\\/browse\\/\\([^ ]*\\)\\)/[\\3](\\2)/g<CR>",
+  { noremap = true }
+)
